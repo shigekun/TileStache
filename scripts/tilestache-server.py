04652b01f99926fb8a682c312648c6b0571793cc
Shigetaka YASUE <shigekun@gmail.com>
2014-10-06 16:40:21 +0900

    Update tilestache-server.py
    run as daemon

--- scripts/tilestache-server.py
+++ scripts/tilestache-server.py
@@ -25,6 +25,15 @@ tile proxied from http://tile.osm.org/0/0/0.png
 Check tilestache-server.py --help to change these defaults.
 """
 
+
+"""sudo easy_install python-daemon"""
+	
+import socket
+import os
+import sys
+from daemon import daemon
+from daemon.pidfile import PIDLockFile
+
 if __name__ == '__main__':
     from datetime import datetime
     from optparse import OptionParser, OptionValueError
@@ -52,6 +61,11 @@ if __name__ == '__main__':
         print >> sys.stderr, "Config file not found. Use -c to pick a tilestache config file."
         sys.exit(1)
 
-    app = TileStache.WSGITileServer(config=options.file, autoreload=True)
-    run_simple(options.ip, options.port, app)
+    with daemon.DaemonContext(pidfile=PIDLockFile(
+	  '/var/run/tilestache-server.pid'),
+	  stdout=sys.stdout,
+	  stderr=sys.stderr
+	):
+        app = TileStache.WSGITileServer(config=options.file, autoreload=True)
+        run_simple(options.ip, options.port, app)
 
